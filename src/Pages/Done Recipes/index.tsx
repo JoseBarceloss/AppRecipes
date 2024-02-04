d270925fcb46cfd65d33890e1886d9103f310341
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-02-04 13:27:33 -0300

    ✨ : sparkles: feat:Requesito 43/47

--- src/Pages/Done Recipes/index.tsx
+++ src/Pages/Done Recipes/index.tsx
@@ -60,8 +60,19 @@ function DoneRecipes() {
             />
             <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
             <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-            <p data-testid={`${index}-horizontal-done-date`}>{recipe.doneDate}</p>
-            
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+
+            <button data-testid={ `${index}-horizontal-share-btn` }>Share</button>
+
+            {recipe.tags.map((tag) => (
+              <span
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                {tag}
+              </span>
+            ))}
+
           </div>
         ))}
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -23,4 +23,5 @@ export type DoneRecipesProps = {
   name: string,
   nationality: string,
   type: string,
+  tags: [string],
 };
